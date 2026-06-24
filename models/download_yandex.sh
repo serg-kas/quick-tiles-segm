9b0ad333fd5b711231764866bc7449aa7dc5f937
serg-kas <serg_kas@mail.ru>
2026-06-24 21:52:50 +0300

    checkpoints file download

--- models/download_yandex.sh
+++ models/download_yandex.sh
@@ -19,7 +19,7 @@ else
 fi
 
 # 
-sam2p1_hiera_l_url="https://downloader.disk.yandex.ru/disk/bad2e17fccdf119c056a76a786484695945432f6c43ef4f7d9baad73d432460f/69effbe6/LzFnBVSI_fpmpGWRTbxW1kbcvbyj02tXOEBISnQytpdE4ANkUDkcjQfDUjGrlD4VzbiO5DwvR6ED737ps8TGIQ%3D%3D?uid=0&filename=sam2.1_hiera_large.pt&disposition=attachment&hash=kj2do0x3QFfdkK4JUq8wfyEHcgB1GNaZOhZLApFOhah48Dxz0Jf1kAxqi6z7dwQtq/J6bpmRyOJonT3VoXnDag%3D%3D%3A&limit=0&content_type=application%2Fzip&owner_uid=271816632&fsize=898083611&hid=cd874b14be76c4ea8e50faaa1dd57bbf&media_type=compressed&tknv=v3"
+sam2p1_hiera_l_url="https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/cMsiauLhyvhsgg"
 
 echo "Downloading sam2.1_hiera_large.pt checkpoint..."
 $CMD $sam2p1_hiera_l_url || { echo "Failed to download checkpoint from $sam2p1_hiera_l_url"; exit 1; }
